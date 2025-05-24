1b7b8e7740325445875096a8dc7e128ed3cefb66
Reid Wagner <wagnerr@umn.edu>
2025-05-24 16:46:27 -0500

    Add diaTracer recipe based on MSFragger and IonQuant recipes.

---
+++ recipes/diatracer/build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Create directories
+TARGET="$PREFIX/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM"
+mkdir -p "$TARGET"
+
+# Add wrapper python script and link to unprefixed name.
+cp "$RECIPE_DIR/diatracer.py" "$TARGET"
+ln -s "$TARGET/diatracer.py" "$PREFIX/bin/diatracer"
+chmod 0755 "${PREFIX}/bin/diatracer"
+
+# Move and link jar.
+mkdir -p "$TARGET/diaTracer-$PKG_VERSION"
+mv "diaTracer-$PKG_VERSION.jar" "$TARGET/diaTracer-$PKG_VERSION"
+cd "$TARGET"
+ln -s "diaTracer-$PKG_VERSION/diaTracer-$PKG_VERSION.jar" "diaTracer.jar"
